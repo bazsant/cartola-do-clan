f26bc43fdac6d3aa9b7dea74df04bce03f7c7d61
Jhones Bazilio dos Santos <bazsantsalesforce@gmail.com>
2020-08-01 20:29:53 -0300

    subindo aplicacao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { RouterModule, Routes } from '@angular/router';
+
+import {MatCardModule} from '@angular/material/card';
+
+import { RegrasComponent } from './regras/regras.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+
+const routes: Routes = [
+  { path: '**', component: RegrasComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegrasComponent,
+    NotFoundComponent,
+    HomeComponent,
+    HeaderComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    NgbModule, 
+    RouterModule.forRoot(routes), 
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
